0336d983a17a0ff5897926b30a3ffc09a85fff82
Ray Raven <rayzafir@aol.com>
2020-12-14 04:11:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // write your code below!
 
+<<<<<<< HEAD
 var name = "Reizy"
 var height = 62
+=======
+var "name" = "Joe"
+var height = "74"
+>>>>>>> a7f6db21a6c5cdbd58f879a374c55c227554171f
 
 console.log("Name:")
 console.log(name)
